1eda232eaa716a3bd2bc15481a5beadb6d99c5f8
smhrd <smhrd@DESKTOP-1HHI0S3>
2021-12-08 11:55:14 +0900

    DAO, Join/Login 수정중

--- 1st Project/src/main/java/Controller/JoinService.java
+++ 1st Project/src/main/java/Controller/JoinService.java
@@ -7,18 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class JoinService
- */
 @WebServlet("/JoinService")
 public class JoinService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		
+	
 	}
 
 }
--- 1st Project/src/main/java/Controller/LoginService.java
+++ 1st Project/src/main/java/Controller/LoginService.java
@@ -7,18 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginService
- */
 @WebServlet("/LoginService")
 public class LoginService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+		
+	
 	}
 
 }
